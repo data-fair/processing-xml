6bd2ddd1188cbcfe20ed41fe7c7e9dbfd0833e1a
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-04-21 15:26:13 +0200

    chore: use eslint

--- index.js
+++ index.js
@@ -2,56 +2,53 @@ const fs = require('fs-extra')
 const path = require('path')
 const util = require('util')
 const FormData = require('form-data')
-const {downloadZip,downloadXML} = require('./lib/download.js')
+const { downloadXML } = require('./lib/download.js')
 const process = require('./lib/process.js')
 
-
 exports.run = async ({ pluginConfig, processingConfig, processingId, dir, tmpDir, axios, log, patchConfig, ws }) => {
-    await downloadXML(processingConfig,tmpDir,axios,log);
-    await process(pluginConfig,processingConfig,tmpDir,axios,log);
+  await downloadXML(processingConfig, tmpDir, axios, log)
+  await process(processingConfig, tmpDir, axios, log)
 
-    const formData = new FormData()
-    formData.append('title', processingConfig.dataset.title)
-    formData.append('extras', JSON.stringify({ processingId }))
-    const filename = path.parse(new URL(processingConfig.url).pathname).name + '.csv'
-    formData.append('file', fs.createReadStream(path.join(tmpDir, filename)), { filename })
-    formData.getLength = util.promisify(formData.getLength)
-    try {
-        let dataset
-        if (processingConfig.datasetMode === 'create') {
-            dataset = (await axios({
-                headers: { ...formData.getHeaders(), 'content-length': await formData.getLength() },
-                method: 'post',
-                url: 'api/v1/datasets/',
-                data: formData,
-                maxContentLength: Infinity,
-                maxBodyLength: Infinity,
-                
-            })).data
-            await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
-            if (processingConfig.datasetMode === 'create') {
-                await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
-            }     
-        } else if (processingConfig.datasetMode === 'update') {
-            await log.step('Vérification du jeu de données')
-            dataset = (await axios.get('api/v1/datasets/' + processingConfig.dataset.id)).data
-            if (!dataset) throw new Error(`Le jeu de données n'existe pas, id="${processingConfig.dataset.id}"`)
-            await log.info(`le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
-            dataset = (await axios({
-                headers: { ...formData.getHeaders(), 'content-length': await formData.getLength() },
-                method: 'post',
-                url: 'api/v1/datasets/' + processingConfig.dataset.id,
-                data: formData,
-                maxContentLength: Infinity,
-                maxBodyLength: Infinity,
-                
-            })).data
-            await log.info(`jeu de donnée mis à jour, id="${dataset.id}", title="${dataset.title}"`)
-        }
-    } catch (err) {
-        console.log(err)
-        console.log(JSON.stringify(err, null, 2))
+  const formData = new FormData()
+  formData.append('title', processingConfig.dataset.title)
+  formData.append('extras', JSON.stringify({ processingId }))
+  const filename = path.parse(new URL(processingConfig.url).pathname).name + '.csv'
+  formData.append('file', fs.createReadStream(path.join(tmpDir, filename)), { filename })
+  formData.getLength = util.promisify(formData.getLength)
+  try {
+    let dataset
+    if (processingConfig.datasetMode === 'create') {
+      dataset = (await axios({
+        headers: { ...formData.getHeaders(), 'content-length': await formData.getLength() },
+        method: 'post',
+        url: 'api/v1/datasets/',
+        data: formData,
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity
+
+      })).data
+      await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
+      if (processingConfig.datasetMode === 'create') {
+        await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+      }
+    } else if (processingConfig.datasetMode === 'update') {
+      await log.step('Vérification du jeu de données')
+      dataset = (await axios.get('api/v1/datasets/' + processingConfig.dataset.id)).data
+      if (!dataset) throw new Error(`Le jeu de données n'existe pas, id="${processingConfig.dataset.id}"`)
+      await log.info(`le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
+      dataset = (await axios({
+        headers: { ...formData.getHeaders(), 'content-length': await formData.getLength() },
+        method: 'post',
+        url: 'api/v1/datasets/' + processingConfig.dataset.id,
+        data: formData,
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity
+
+      })).data
+      await log.info(`jeu de donnée mis à jour, id="${dataset.id}", title="${dataset.title}"`)
     }
-      
-    
+  } catch (err) {
+    console.log(err)
+    console.log(JSON.stringify(err, null, 2))
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,57 +1,53 @@
-const config = require('../config/local-dev');
-const testUtils = require('@data-fair/processings-test-utils');
-const {downloadZip,downloadXML} = require('../lib/download.js');
-const process = require('../lib/process.js');
-const xmlProcessing = require('../');
-
+const config = require('../config/local-dev')
+const testUtils = require('@data-fair/processings-test-utils')
+const { downloadZip, downloadXML } = require('../lib/download.js')
+const process = require('../lib/process.js')
+const xmlProcessing = require('../')
 
 describe('Download', function () {
-    it('should download a zip', async function () {
-      const context = testUtils.context({
-        pluginConfig: {
-          
-        },
-        processingConfig: {
-          url: 'https://donnees.roulez-eco.fr/opendata/instantane'
-        },
-        tmpDir: 'data/'
-      }, config, false);
-      await downloadZip(context.processingConfig, context.tmpDir, context.axios, context.log);
-    });
-    it('should download an XML file', async function () {
-      this.timeout(10000);
+  it('should download a zip', async function () {
+    const context = testUtils.context({
+      pluginConfig: {
 
-      const context = testUtils.context({
-        pluginConfig: {
-        },
-        processingConfig: {
-          url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2020',
-        },
-        tmpDir: 'data/'
-      }, config, false);
-      await downloadXML(context.processingConfig,context.tmpDir,context.axios,context.log);
-    });
+      },
+      processingConfig: {
+        url: 'https://donnees.roulez-eco.fr/opendata/instantane'
+      },
+      tmpDir: 'data/'
+    }, config, false)
+    await downloadZip(context.processingConfig, context.tmpDir, context.axios, context.log)
+  })
+  it('should download an XML file', async function () {
+    this.timeout(10000)
 
-});
+    const context = testUtils.context({
+      pluginConfig: {
+      },
+      processingConfig: {
+        url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2020'
+      },
+      tmpDir: 'data/'
+    }, config, false)
+    await downloadXML(context.processingConfig, context.tmpDir, context.axios, context.log)
+  })
+})
 
 describe('Process', function () {
   it('should create a csv from an xml file', async function () {
     const context = testUtils.context({
       pluginConfig: {
-        
+
       },
       processingConfig: {
         datasetMode: 'create',
         url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2020',
-        separateur : '-'
+        separateur: '-'
       },
       tmpDir: 'data/'
-    }, config, false);
-    await process(context.pluginConfig,context.processingConfig, context.tmpDir, context.axios, context.log);
-  });
-  
-  
-});
+    }, config, false)
+    await process(context.pluginConfig, context.processingConfig, context.tmpDir, context.axios, context.log)
+  })
+})
 
 describe('Load csv test', function () {
   it('should load a csv on the staging', async function () {
@@ -61,17 +57,17 @@ describe('Load csv test', function () {
       },
       processingConfig: {
         datasetMode: 'create',
-        dataset :{title : 'xmlProcessing test'},
+        dataset: { title: 'xmlProcessing test' },
         url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2020',
-        separateur : '-'
+        separateur: '-'
       },
       tmpDir: 'data/'
-    }, config, false);
-    await xmlProcessing.run(context);
+    }, config, false)
+    await xmlProcessing.run(context)
   })
-});
+})
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('Another load csv test', function () {
   it('should load a csv on the staging', async function () {
@@ -81,15 +77,15 @@ describe('Another load csv test', function () {
       },
       processingConfig: {
         datasetMode: 'create',
-        dataset :{id : 'xmlprocessing-test-update', title : 'xmlProcessing test update'},
+        dataset: { id: 'xmlprocessing-test-update', title: 'xmlProcessing test update' },
         url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2020',
-        separateur : '-'
+        separateur: '-'
       },
       tmpDir: 'data/'
-    }, config, false);
-    await xmlProcessing.run(context);
+    }, config, false)
+    await xmlProcessing.run(context)
   })
-});
+})
 
 describe('Update dataset', function () {
   it('should update a dataset on the staging', async function () {
@@ -99,14 +95,13 @@ describe('Update dataset', function () {
       },
       processingConfig: {
         datasetMode: 'update',
-        dataset :{id : 'xmlprocessing-test-update', title : 'xmlProcessing test update'},
+        dataset: { id: 'xmlprocessing-test-update', title: 'xmlProcessing test update' },
         url: 'https://data-api.megalis.bretagne.bzh/api/v1/decp/222200016/2023',
-        separateur : '-'   
+        separateur: '-'
       },
       tmpDir: 'data/'
-    }, config, false);
-    await wait(15000);
-    await xmlProcessing.run(context);
-  });
-});
-
+    }, config, false)
+    await wait(15000)
+    await xmlProcessing.run(context)
+  })
+})
